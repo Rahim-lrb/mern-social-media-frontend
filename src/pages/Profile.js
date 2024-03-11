6952c44e6867b55cc302631956a63a83b6ef9215
Rahim-lrb <aberrahimcoder@gmail.com>
2024-03-11 20:33:55 +0100

    responsive profile start

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -36,7 +36,7 @@ const Profile = (props) => {
 
     return (
         <div>
-            <div className="p-16">
+            <div className="p-2 lg:p-16">
                 <div className="p-8 bg-white shadow mt-24">
                     <div className="grid grid-cols-1 md:grid-cols-3">
                         <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
